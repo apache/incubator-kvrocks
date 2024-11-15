9d9aa80ea3f82f1d443089acbb47df91f12a3af1
PragmaTwice <twice@apache.org>
2024-11-15 22:53:57 +0800

    feat(cmd): all blocking commands should be no-multi and no-script

--- src/commands/commander.h
+++ src/commands/commander.h
@@ -328,9 +328,12 @@ inline uint64_t ParseCommandFlags(const std::string &description, const std::str
       flags |= kCmdNoDBSizeCheck;
     else if (flag == "slow")
       flags |= kCmdSlow;
-    else if (flag == "blocking")
+    else if (flag == "blocking") {
       flags |= kCmdBlocking;
-    else {
+
+      // blocking commands should always be no-multi and no-script
+      flags |= kCmdNoMulti | kCmdNoScript;
+    } else {
       std::cout << fmt::format("Encountered non-existent flag '{}' in command {} in command attribute parsing", flag,
                                cmd_name)
                 << std::endl;
