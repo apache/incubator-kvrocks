c2e00d38fbca74ecfb93afb93d1b57a297aa19ba
Aleks Lozovyuk <aleks.raiden@gmail.com>
2025-01-03 00:44:44 +0300

    Update rocksdb.cmake

--- cmake/rocksdb.cmake
+++ cmake/rocksdb.cmake
@@ -27,7 +27,7 @@ include(cmake/utils.cmake)
 
 FetchContent_DeclareGitHubWithMirror(rocksdb
   facebook/rocksdb v9.10.0
-  MD5=ccc55dbd6f89d41c5bf2cb5a4c83a71
+  MD5=ccc55dbd6f89d41c5bf2cb5a4c83a714
 )
 
 FetchContent_GetProperties(jemalloc)
