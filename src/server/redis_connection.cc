80c6fda072bc559249bd452c68f4316a1959c58f
PragmaTwice <twice@apache.org>
2025-02-03 23:54:28 +0800

    fix

--- src/server/redis_connection.cc
+++ src/server/redis_connection.cc
@@ -389,8 +389,8 @@ void Connection::ExecuteCommands(std::deque<CommandTokens> *to_process_cmds) {
       if (util::EqualICase(cmd_name, "host:") || util::EqualICase(cmd_name, "post")) {
         LOG(WARNING) << "A likely HTTP request is detected in the RESP connection, indicating a potential "
                         "Cross-Protocol Scripting attack. Connection aborted.";
-        owner_->FreeConnection(this);
-        break;
+        Close();
+        return;
       }
       if (is_multi_exec) multi_error_ = true;
       Reply(redis::Error(
